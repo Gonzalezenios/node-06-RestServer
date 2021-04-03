d32a0093001d5f7a54655b9c6cfeba2684ed9bce
Enio Gonzalez <gonzalezeniood@gmail.com>
2021-04-02 21:31:55 -0300

    levantamos la app

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config;
+const express = require('express');
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+});
+
+app.listen(port, ()  => {
+    console.log(`############# App started. Port: ${port} #############`);
+})

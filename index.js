3e4ba193306ec632baf8a14aa181e87ee8b5ba71
Enio Gonzalez <gonzalezeniood@gmail.com>
2021-04-02 22:18:44 -0300

    se configuran los metodos http

--- index.js
+++ index.js
@@ -4,8 +4,71 @@ const app = express()
 
 const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.use(express.json());
+
+app.get('/users', function (req, res) {
+    const users = [
+        {
+            id: 1,
+            name: 'Enio'
+        },
+        {
+            id: 2,
+            name: 'Florencia'
+        },
+    ]
+    res.json(users);
+});
+
+app.post('/users',  (req, res) => {
+
+    
+    const user = req.body;
+    user.id = 123
+
+    const result = {
+        message: 'User Created',
+        user
+    }
+    res.status(201).json(result);
+});
+
+app.put('/users/:id', (req, res) => {
+
+    const { id } = req.params;
+    const user = req.body;
+
+    user.id = id;
+
+
+    const result = {
+        message: 'User Updated',
+        user
+    }
+    res.json(result);
+});
+
+app.patch('/users/:id', (req, res) => {
+
+    const { id } = req.params;
+    const user = req.body;
+
+    user.id = id;
+
+    const result = {
+        message: 'User updated with patch',
+        user
+    }
+    res.json(result);
+});
+
+app.delete('/users/:id', (req, res) => {
+
+    const { id } = req.params;
+    const result = {
+        message: `User with id:${id} Deleted`
+    }
+    res.json(result);
 });
 
 app.listen(port, ()  => {
